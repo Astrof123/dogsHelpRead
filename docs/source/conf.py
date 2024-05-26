a5d6ddfd40cac56d1da089cde9e9b481a12ca766
Astrof123 <71438030+Astrof123@users.noreply.github.com>
2024-05-26 12:29:07 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'dogsHelp'
+copyright = '2024, Gleb'
+author = 'Gleb'
 
 release = '0.1'
 version = '0.1.0'
